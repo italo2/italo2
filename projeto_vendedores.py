074b22d820beaa53f13b5d2fe1b2188c673db1b1
italo A <alvesitalo_1@hotmail.com>
2021-09-08 11:01:06 -0300

    Add files via upload

--- projeto_vendedores.py
+++ projeto_vendedores.py
@@ -4,27 +4,46 @@ Sistema de comissão de uma loja !
 by italo a.
 
 aprendizado de python if e else .
+sistema de and,or,not , is
 """
+user=int(input('digite seu login :  : !'))
+login=int(input('digite sua senha : '))
 
-valor_base_produto =float(input('Digite o valor do produto base :  '))
+if user == 5555 and login == 256 :
 
-desconto_avista=valor_base_produto * -0.10 + valor_base_produto
+    ativo = True
+    logado = True
+else :
 
-input(f'valor com o desconto : {desconto_avista}')
+   ativo = False
+   logado = False
 
-valor_parcelado_sjuros=(input(f'valor da parcela em 3 vezes sem juros : {valor_base_produto / 3}'))
+if ativo and logado :
 
-venda_vista=int(input( ' A venda realizada foi a vista sim (1) ou não (2) : '))
+    valor_base_produto =float(input('Digite o valor do produto base :  '))
 
+    desconto_avista=valor_base_produto * -0.10 + valor_base_produto
 
+    input(f'valor com o desconto : {desconto_avista}')
 
-if venda_vista == 1:
-    input(f'sua escolha foi de venda realizada à vista : {venda_vista}')
-    valo_comissao=0
+    valor_parcelado_sjuros=(input(f'valor da parcela em 3 vezes sem juros : {valor_base_produto / 3}'))
 
-    valor_comissao=input(f'sua comissao será de : {desconto_avista * 0.05 }')
-else:
+    venda_vista=int(input( ' A venda realizada foi a vista sim (1) ou não (2) : '))
 
+    if venda_vista == 1:
+        input(f'sua escolha foi de venda realizada à vista : {venda_vista}')
+        valo_comissao=0
 
+        valor_comissao=input(f'sua comissao será de : {desconto_avista * 0.05 }')
 
- exit(input(f'sua comissão sobre a venda à vista é : {valor_base_produto * 0.05 }'))
+        exit('Deslogado ..')
+
+    else:
+
+        exit(input(f'sua comissão sobre a venda à vista é : {valor_base_produto * 0.05 }'))
+        exit('Deslogado ..')
+
+else :
+
+   exit("não logado ! ")
+
